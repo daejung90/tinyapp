c1351a1470ebb80b26d1e2e58d4caad8791937af
daejung90 <djung.ca@gmail.com>
2022-11-30 10:17:01 -0500

    views commit

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080 (usually when is working in localhost)
 
+app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,7 +21,15 @@ app.get("/urls/json", (req, res) => {
     res.json(urlDatabase)
 })
 
+app.get("/urls", (req, res) => {
+    const templateVars ={ url: urlDatabase};
+    res.render("urls_index", templateVars);
+})
+
 app.get("/hello", (req, res) => {
     res.send(res.send("<html><body>Hello <b>World<b></body></html>\n"))
 })
-
+// app.get("/hello", (req, res) => {
+//     const templateVars = { greeting: "Hello World!" };
+//     res.render("hello_world", templateVars);
+//   });
