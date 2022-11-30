bf420106d8b3c1b16d474c7304daf51a5e9ffdf9
daejung90 <djung.ca@gmail.com>
2022-11-30 12:55:19 -0500

    refactoring url shortening, and GET and POST to submit a form

--- express_server.js
+++ express_server.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 8080; //default port 8080 (usually when is working in localhost)
 
+app.use(express.urlencoded ({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < 6; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return result
+}
 
 app.get("/", (req, res) => {
     res.send("Hello")
@@ -29,12 +39,21 @@ app.get("/urls", (req, res) => {
 app.get("/hello", (req, res) => {
     res.send(res.send("<html><body>Hello <b>World<b></body></html>\n"))
 })
-// app.get("/hello", (req, res) => {
-//     const templateVars = { greeting: "Hello World!" };
-//     res.render("hello_world", templateVars);
-//   });
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new")
+});
 
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
     res.render("urls_show", templateVars);
-  });
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body);
+    res.send("Ok")
+})
+
+
+
+
